51303a920b04d5eecf26ae972b77f9ce7f488379
Ben Rosen <benjamin.p.rosen@gmail.com>
2023-05-24 17:43:57 -0400

    configure package scripts

---
+++ modules/infrastructure/masquerade/masquerade.ts
@@ -0,0 +1,8 @@
+import { Stack } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class Masquerade extends Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+  }
+}
